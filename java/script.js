d17a815d5ce6a6a7a66217c18e323caf1134605b
HerBaXs <ern.herbaxs@gmail.com>
2022-01-08 04:53:28 +0400

    first commit

---
+++ java/script.js
@@ -0,0 +1,28 @@
+function myAction() {
+    var lastName = "Abdulrahman";
+    var lastSurName = "Gurbanzade";
+    var lastAge = "19";
+    var lastEducation = "Programmer";
+    var lastHobby = "Write code";
+    
+    document.write("Salam Mən "+lastName+" "+lastSurName+" "+lastAge+" "+lastEducation+" "+lastHobby+" ");
+    
+    console.log("Salam Mən",lastName+"",lastSurName+"",lastAge+"",lastEducation+"",lastHobby+" ");
+    }
+    
+    
+    function myAbout() {   
+     let a = "Abdulrahman";
+     let b = "Gurbanzade";
+     let c = "19 Yaşım var.";
+     let d = "Mənim Təhsilim Programmerdir";
+     let e = "Mənim Hobbim isə Kod yazmağdır";
+    
+      document.write("Salam Mən "+a+" "+b+" "+c+" "+d+" "+e+"");
+    
+      console.log("Salam Mən",a+"",b+"",c+"",d+"",e+"");
+    
+    
+      // document.getElementById("classid").innerHTML = "Salam Mən" + " " "Abdulrahman" + " " + "Gurbanzade" + " " + "Tehsilim Programci dir" + " " + "Ve Menim hobbim kod yazmagdir";
+    }
+    
